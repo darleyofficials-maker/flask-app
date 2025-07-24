6ae3cd8fda7331be18e6d1008afc4c9331d16efc
darleyofficials-maker <darleyofficials@gmail.com>
2025-07-24 05:37:40 +0300

    Flask app

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = 'vikkies-super-secret'
 
 # DB connection
 def get_db_connection():
